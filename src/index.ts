a212b8057255b4d0c95a69fe73fdea4984402e56
Alexander Trauzzi <acj@trauzzi.me>
2020-03-08 13:07:18 -0500

    Include configuration dictionary for hinting.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { PouchDb, PouchDbProps, PouchDbConsumer, PouchDbProvider, pouchDbContext, ConfigurationWithInstance, ConfigurationWithoutInstance } from "./PouchDb";
+export { PouchDb, PouchDbProps, PouchDbConsumer, PouchDbProvider, pouchDbContext, ConfigurationWithInstance, ConfigurationWithoutInstance, DatabaseConfigurationDictionary } from "./PouchDb";
 export { usePouchDbFind } from "./PouchDbFind";
