cba29ddcab1fed8bb373e0b28533c597d3ec170d
Alexander Trauzzi <acj@trauzzi.me>
2020-03-08 11:51:34 -0500

    Remember to export props.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { PouchDb, PouchDbConsumer, PouchDbProvider, pouchDbContext } from "./PouchDb";
+export { PouchDb, PouchDbProps, PouchDbConsumer, PouchDbProvider, pouchDbContext } from "./PouchDb";
 export { usePouchDbFind } from "./PouchDbFind";
